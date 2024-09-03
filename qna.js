584ef149831645a5a4d275ebce6ab2b23238fbda
abarlukova <32097733+abarlukova@users.noreply.github.com>
2024-09-03 11:53:04 +0000

    qna

---
+++ qna.js
@@ -0,0 +1,87 @@
+[
+  {
+    "section": "keto diet",
+    "documents": [
+      {
+        "question": "Is a ketogenic diet good for weight loss?",
+        "answer": "Research has shown a ketogenic diet can induce rapid weight loss, partly from water loss but also some fat loss. However, the “weight loss effect becomes similar to other dietary approaches after one year,” according to a recent review of low-carb diets. Taking in less energy than you burn will lead to weight loss. The National Diet & Nutrition Survey reports that on average we get almost 50% of our energy from carbs. Cutting this by at least half is likely to reduce total calorie intake, even if it’s replaced with fats. On the other hand, if you eat more calories from fat than your body needs, it will still be stored as fat."
+      },
+      {
+        "question": "What is the keto diet?",
+        "answer": "The ketogenic diet is a very low-carb, high-fat diet that shares many similarities with low-carb diets. It involves drastically reducing carbohydrate intake and replacing it with fat. This reduction in carbs puts your body into a metabolic state called ketosis. When this happens, your body becomes incredibly efficient in burning fat for energy. It also turns fat into ketones in the liver, which can supply power to the brain. Ketogenic diets can cause significant reductions in blood sugar and insulin levels. This, along with the increased ketones, has some health benefits."
+      },
+      {
+        "question": "Is ketogenic diet good for your body?",
+        "answer": "While the ketogenic diet may be recommended for some people with uncontrolled epilepsy, the high-fat content — and especially the high level of unhealthy saturated fat — combined with limits on nutrient-rich fruits, veggies, and grains is a concern for long-term heart health."
+      },
+      {
+        "question": "What is a good keto meal plan?",
+        "answer": "A good keto meals plan should include the following foods: Eggs: pastured, organic, or conventional all work fine; Poultry: chicken and turkey; Fatty fish: salmon, herring, and mackerel; Meat: beef & organ meats; Full-fat dairy: unsweetened yogurt, butter, and cream; Full-fat cheese: cheddar, mozzarella, brie, goat cheese, and cream cheese; Nuts and seeds: macadamia nuts, almonds, walnuts, pumpkin seeds and flaxseeds; Nut butter: no-sugar-added peanut, almond, and cashew butter; Oils rich in healthy fats: olive oil, avocado oil, and sesame oil; Avocados: whole avocados can be added to almost any meal or snack; Non-starchy vegetables: greens, broccoli, tomatoes, mushrooms, and peppers; Condiments: salt, pepper, vinegar, lemon juice, fresh herbs, and spices."
+      },
+      {
+        "question": "Can I lose 20 pounds in one month on a keto diet?",
+        "answer": "Studies say that losing weight on the ketogenic diet is below points: 1) One study found obese patients lost 13.6 kg (30 pounds) after 2 months on the keto diet, and over 88% of patients lost more than 10% of their initial weight by the end of the study. Lean mass was practically unaffected. That is 3.5 pounds of pure fat per week. 2) Another study found that obese patients weighing 101 kg dropped 10 kg (22 pounds) after 8 weeks. They lost an extra 2 kg (4.4 pounds) by week 16 and 3 more kg (6.6 pounds) by week 24. In total, they lost 15 kg (33 pounds) in 5.5 months. That is 1.3 pounds per week. 3) One study of volunteers with obesity and type 2 diabetes who weighed 108 kg dropped 11.1 kg (24.5 pounds) in 24 weeks. That’s 1 pound per week. 4) A fourth study found 120 overweight hyperlipidemia patients lost 9.4 kg (20.7 pounds) of fat mass in 24 weeks. That is 0.8 pounds per week. 5) A meta-analysis that took data from 13 different studies found patients consistently lost more weight on the ketogenic diet than on a low-fat diet."
+      },
+      {
+        "question": "What are the benefits of a keto diet?",
+        "answer": "Supports weight loss, Improves acne, May reduce the risk of certain cancers, May improve heart health, May protect brain function, Potentially reduces seizures, Improves PCOS symptoms, Risks and complications"
+      },
+      {
+        "question": "What has been your experience with the ketogenic diet?",
+        "answer": "The keto diet plan is a high-fat and low-carb (HFLC) diet. I would actually describe it as extremely low-carb—you are allowed to eat just 20 grams in a day. Some people on keto follow a net-carb plan (you can subtract the grams of fiber from a food's total carbs) and you allow eating more carbs in a day. For my 14-day diet and for the sake of simplicity, I stuck with total carbs. As a rule, I aimed for 20 grams each day—2 at breakfast, 5 at lunch, 3 for snacks, and 10 for dinner. I found that if I aimed for 20, I would land under 30. That was successful enough for me. The key to hitting my number was to plan, plan, and plan. I worked out all three meals, condiments, and snacks on the weekends. If I knew what I was having and what I was \"allowed\" to have while staying under my carb goal, I found managing the infrequent cravings and hunger pangs easier. I cannot stress enough the importance of planning for a keto diet."
+      },
+      {
+        "question": "How long does it take to get results from the keto diet?",
+        "answer": "Whether you are following a keto or any other diet plan that puts you in a caloric deficit, it can take anywhere between a week to several weeks to start seeing results, depending on a number of different factors. This article will discuss the keto diet. Also, how it helps you in losing weight. Some advice for choosing foods on keto, and some important potential side effects you should consider before fully committing to the keto eating style."
+      },
+      {
+        "question": "Is keto dieting safe?",
+        "answer": "The keto diet could cause low blood pressure, kidney stones, constipation, nutrient deficiencies, and an increased risk of heart disease. Strict diets like keto could also cause social isolation or disordered eating. Keto is not safe for those with any conditions involving their pancreas, liver, thyroid, or gallbladder. Someone new to the keto diet can also experience what has been called the “keto flu” with symptoms like upset stomach, dizziness, decreased energy, and mood swings caused by your body adapting to ketosis."
+      },
+      {
+        "question": "What are the advantages and disadvantages of a ketogenic diet?",
+        "answer": "ADVANTAGES: Weight loss, Lower cholesterol, Reduces risk of heart disease, Lowers blood pressure, Diabetes-friendly & Cognition friendly. DISADVANTAGES: Getting used to it, Bowel complaints, It may feel restrictive & High cholesterol."
+      },
+      {
+        "question": "How does the ketogenic diet work?",
+        "answer": "A typical American diet consists of 50% or more carbohydrates, which convert to glucose in the body. Your cells burn that glucose as fuel. But when you switch to a very high-fat, low-carb diet, your body, by necessity, shifts away from glucose and instead uses fatty acids and ketone bodies for energy. This process is called ketosis — hence the diet's name."
+      },
+      {
+        "question": "How does a Ketogenic diet change your life?",
+        "answer": "In ketosis, you will feel less hungry as your body runs off the ketones in your bloodstream and the fats you are consuming. The Optima keto diet could cause low blood pressure, kidney stones, constipation, nutrient deficiencies, and an increased risk of heart disease. Strict diets like keto could also cause social isolation or disordered eating. Keto is not safe for those with any conditions involving their pancreas, liver, thyroid, or gallbladder. Ketone levels can increase your energy as well as improve brain health, gut function, and hormone balance."
+      },
+      {
+        "question": "Is it possible to gain muscles on ketogenic diet?",
+        "answer": "A study of 25 college-aged men compared a traditional Western diet against the ketogenic diet for muscle gain, strength, and performance, and found that both diets were equally effective. Other studies have shown that keto may provide similar strength and performance gains as a conventional high-carb diet while also helping shed body fat. Still, if you are new to keto, you may initially experience a drop in strength and performance. It is important to note that this drop is often temporary and happens because your body is adapting to relying on ketones."
+      },
+      {
+        "question": "What can't you eat on a keto diet?",
+        "answer": "Grains, Starchy vegetables and high-sugar fruits, Sweetened yogurt, Juices, Honey, syrup or sugar in any form, Chips and crackers, Baked goods including gluten-free baked goods."
+      },
+      {
+        "question": "What is the Indian vegetarian version of keto diet?",
+        "answer": "Kabobs, kababs, or kebabs, Chicken, sheep, fish, or paneer bhurji tikka, Egg or Paneer Bhurji, Roasted Chicken Salad, Paneer tikka or baked, Baingan Bharta, Many curry."
+      },
+      {
+        "question": "What is the best custom keto diet plan?",
+        "answer": "A ketogenic diet should consist of about 60–80% fat, 10–30% protein, and no more than 5–10% — or 20–50 grams — of carbs per day. Focus on high-fat, low-carb foods like eggs, meats, dairy and low-carb vegetables, as well as sugar-free beverages. Be sure to limit highly processed foods and Trans fats."
+      },
+      {
+        "question": "Is the keto diet healthy over a long period of time?",
+        "answer": "It is unclear how effective keto is in the long term, and — as with all restrictive diets — there is a good chance you will put the weight back on when you stop following it. Keto can have health benefits for people, including those with obesity, those trying to control their blood sugar levels, and people with certain hard-to-treat forms of epilepsy. However, if you are not working with a healthcare professional, the keto diet may come with health risks. In addition, if you do not carefully manage what you eat, you could miss important nutrients, including fiber. Before starting any diet, you should consider whether you actually need to lose weight. Work with a healthcare professional to find the weight range that is best for your health."
+      },
+      {
+        "question": "How do I start a keto diet the easy way?",
+        "answer": "a) Know What Foods You Can Eat and Which to Avoid, b) Be Prepared to Eat Lots of Fat, c) Know How Much Protein to Eat (it is Probably Less Than You Think!), d) Get Familiar with Reading Nutrition Labels, e) Stay Hydrated, f) Know That You May Experience the “Keto Flu”, g) Meal Plan Like a Pro."
+      },
+      {
+        "question": "For how many months should I follow keto diet?",
+        "answer": "While some people have success staying on keto for an extended period of time, “the long-term research is limited,” says Jill Gulotta, RDN, who is based in White Plains, New York. Gulotta recommends staying on keto for six months max before reintroducing more carbs to your diet."
+      },
+      {
+        "question": "Why is a ketogenic diet becoming popular?",
+        "answer": "There is no denying that the keto diet is popular, but the ride has been controversial. Much of it is thanks to the diet culture 180-degree turn from fat-free everything to the rapid rise of high-fat and extremely low-carb diets. Despite the criticism, the keto diet is well supported by scientific evidence. Here is what the latest research tells us about the keto diet statistics and facts."
+      }
+    ]
+  }
+]
